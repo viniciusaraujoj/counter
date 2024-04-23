2c0467cd16d8f62187291ca19d2bc59ef79267ef
Vinicius Araujo <viniciusaraujoaj@gmail.com>
2024-04-23 01:26:09 -0300

    Adicionado foreach no codigo

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
+// count
+let value = 0;
+
+// values and buttons
 const counter = document.querySelector(".counter");
-const decrease = document.querySelector(".decrease");
-const reset = document.querySelector(".reset");
-const increase = document.querySelector(".increase");
+const btns = document.querySelectorAll(".btn");
 
-decrease.addEventListener("click", function () {
-  let number = Number(counter.textContent);
-  number--;
-  counter.textContent = number;
-  numberColor(number);
-});
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("increase")) {
+      value++;
+    } else if (styles.contains("decrease")) {
+      value--;
+    } else {
+      value = 0;
+    }
 
-increase.addEventListener("click", function () {
-  let number = Number(counter.textContent);
-  number++;
-  counter.textContent = number;
-
-  numberColor(number);
-});
-
-reset.addEventListener("click", function () {
-  let number = Number(counter.textContent);
-  counter.textContent = 0;
-  numberColor();
+    counter.textContent = value;
+    numberColor(value);
+  });
 });
 
 function numberColor(number) {
+  let colorCounter = counter.style.color;
   if (number > 0) {
     counter.style.color = "#008000";
   } else if (number < 0) {
